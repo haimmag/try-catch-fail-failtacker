09bbb81c854563fad53342dfbe763a1123a34811
haimamg <haim.magen@gmail.com>
2014-11-02 15:28:53 +0200

    added links services and construct them

--- Conduit.Mobile.ControlPanelV2.MarketingAdvisor/Scripts/services/app-mobile-links.srv.js
+++ Conduit.Mobile.ControlPanelV2.MarketingAdvisor/Scripts/services/app-mobile-links.srv.js
@@ -1,21 +1,47 @@
-app.service('AppMobileLinksService', ['$rootScope', '$http', '$q', 'CommonService', 'AmsService', function ($rootScope, $http, $q, CommonService, AmsService) {
+(function () {
     'use strict';
 
-    var config = CommonService.config;
-    var appId = config.productionAppId;
+    var app = angular.module('app');
 
-    return {
-        getCustomLinks: getCustomLinks
-    };
+    app.service('AppMobileLinksService', [
+        '$rootScope', '$http', '$q', 'CommonService', 'AmsService', function ($rootScope, $http, $q, CommonService, AmsService) {
+            'use strict';
 
-    function getCustomLinks() {
-        var deferred = $q.defer();
+            var config = CommonService.config;
+            var appId = config.productionAppId;
 
-        AmsService.getAppData(appId).then(function (appData) {            
-            deferred.resolve(appData);
-        });        
+            return {
+                getCustomLinks: getCustomLinks
+            };
 
-        return deferred.promise;
-    }
+            function getCustomLinks() {
+                var deferred = $q.defer();
 
-}]);
+                AmsService.getAppData(appId).then(function (appData) {
+                    var links = prepareLinks(appData);
+                    deferred.resolve(links);
+                });
+
+                return deferred.promise;
+            }
+
+            function prepareLinks(appData) {
+                var baseUrl = appData.result.details.appHomeUrl;
+                var links = [];
+
+                var pages = appData.result.pages;
+
+                links.push("#");
+
+                for (var i = 0; i < pages.length; i++) {
+                    links.push("#" + pages[i].alias);
+                }
+
+                return {
+                    links: links,
+                    baseUrl: baseUrl
+                };
+            }
+        }]);
+})();
+//# sourceMappingURL=app-mobile-links.srv.js.map
