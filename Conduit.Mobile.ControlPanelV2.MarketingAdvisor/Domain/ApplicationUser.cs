7dbf6346049555d9c86a438bdc9fffe9ff5d66b6
haimamg <haim.magen@gmail.com>
2015-01-11 12:19:43 +0200

    db

--- Conduit.Mobile.ControlPanelV2.MarketingAdvisor/Domain/ApplicationUser.cs
+++ Conduit.Mobile.ControlPanelV2.MarketingAdvisor/Domain/ApplicationUser.cs
@@ -7,6 +7,6 @@ namespace Conduit.Mobile.ControlPanelV2.External.Domain
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-	    public IList<Issue> Assignments { get; set; }
+        public IList<Issue> Assignments { get; set; }
     }
 }
